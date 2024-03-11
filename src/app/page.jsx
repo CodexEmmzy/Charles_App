07a680c9d8bfcff6546203fa95e5a4071d478f5c
Charles Emmanuel <emmaxcharles123@gmail.com>
2024-03-11 06:23:05 -0700

    Fixing dev bug

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,23 @@
-import styles from "./homepage.module.css";
-import Featured from "@/components/featured/Featured";
-import CategoryList from "@/components/categoryList/CategoryList";
-import CardList from "@/components/cardList/CardList";
-import Menu from "@/components/Menu/Menu";
+"use client"
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router'; // Import useRouter from 'next/router'
+import Featured from '@/components/featured/Featured';
+import CategoryList from '@/components/categoryList/CategoryList';
+import CardList from '@/components/cardList/CardList';
+import Menu from '@/components/Menu/Menu';
 
-export default function Home({ searchParams }) {
-  const page = parseInt(searchParams.page) || 1;
+export default function Home() {
+  const router = useRouter();
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    // Check if useRouter is available (client-side)
+    if (router.isReady) {
+      const { query } = router;
+      const pageQueryParam = parseInt(query.page) || 1;
+      setPage(pageQueryParam);
+    }
+  }, [router.isReady, router.query]);
 
   return (
     <div className={styles.container}>
